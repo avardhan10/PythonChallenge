d8cb8eb84ef4a52012649fb2867cd973c8e5c381
Anjali Vardhan <anjalivardhan@gmail.com>
2018-11-14 22:16:42 -0800

    Calculate average change in P/L

--- Pybank/main.py
+++ Pybank/main.py
@@ -17,3 +17,8 @@ with open('budget_data.csv') as csvfile:
         netEarnings += int(row[1])
         months.append(row[0])
         earnings.append(int(row[1]))
+
+# in the range of net earnings from the second month to the end, calculating the greatest increase and decrease
+    for entry in range(1, len(earnings)):
+        averageChange += earnings[entry] - earnings[entry - 1]
+        currentChange = earnings[entry] - earnings[entry - 1]
