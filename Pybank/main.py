c3e962d20fa7d36b8ea34453bcc2972de0e0027b
Anjali Vardhan <anjalivardhan@gmail.com>
2018-11-14 22:11:12 -0800

    Read months and earnings data into lists

--- Pybank/main.py
+++ Pybank/main.py
@@ -8,3 +8,12 @@ netEarnings = 0
 averageChange = 0
 changeMonth = ['', '']
 changeAmount = [0, 0]
+
+# Opens input csv file and reads data row by row
+with open('budget_data.csv') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)
+    for row in csvreader:
+        netEarnings += int(row[1])
+        months.append(row[0])
+        earnings.append(int(row[1]))
