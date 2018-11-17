507d53c344147a84e0fed8ee8fc980985088f5bc
Anjali Vardhan <anjalivardhan@gmail.com>
2018-11-16 18:25:06 -0800

    Pypoll: Count total votes and vote count for each candidate

--- Pypoll/main.py
+++ Pypoll/main.py
@@ -12,3 +12,11 @@ voteCount = {}
 with open('election_data.csv') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next(csvreader)
+    for row in csvreader:
+        # Counting total number of votes casted by adding 1 to count for each row in csv
+        numOfVotes += 1
+        # Counting votes for each candidate. If dictionary does not already contain candidate (identified by name), add it to dictionary. For candidate in row, add 1 to existing count
+        candidateName = row[2]
+        if candidateName not in voteCount:
+            voteCount[candidateName] = 0
+        voteCount[candidateName] += 1
