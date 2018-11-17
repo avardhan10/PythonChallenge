eb43a33931350188d0bc7bcea2ec4d3df7747535
Anjali Vardhan <anjalivardhan@gmail.com>
2018-11-16 18:22:54 -0800

    Pypoll: Read input file row by row

--- Pypoll/main.py
+++ Pypoll/main.py
@@ -7,3 +7,8 @@ winner = ''
 
 # Setting up dictionary
 voteCount = {}
+
+# Opens input csv file and reads data row by row
+with open('election_data.csv') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)
